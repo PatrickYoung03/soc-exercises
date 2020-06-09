2533d55728d4bc9b9b5de4032ed82d6d5fa49533
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-06-09 13:47:12 +0100

    final changes

--- exercise2.js
+++ exercise2.js
@@ -6,5 +6,8 @@ var arr2 = ["Javascript", "is", "great"];
 
 // 2. convert this new array to be one string
 
-// 3. arrange these numbers into order from lowest to highest and print the new array in the console
-var numbers = [12, 20, 30, 2, 123, 2, 5];
+// 3. swap the first 2 cities in the cities array to be 2 new cities of your choice
+var cities = ["Birmingham", "London", "Lichfield", "Manchester", "Glasgow"];
+
+// 4. arrange these numbers into order from lowest to highest and print the new array in the console
+var numbers = [12, 20, 30, 2, 123, 2, 5, 2000, 43, 43, 56];
